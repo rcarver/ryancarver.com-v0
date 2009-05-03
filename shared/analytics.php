9338241b2402db5447c655c19108acb5c8a5b24f
Ryan Carver <ryan@fivesevensix.com>
2009-05-03 11:33:16 -0700

    turn GA on for live

--- shared/analytics.php
+++ shared/analytics.php
@@ -1,8 +1,8 @@
-<?php if (false) { ?>
+<?php if ($_SERVER['HTTP_HOST'] != "localhost"): ?>
   <script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
   </script>
   <script type="text/javascript">
   _uacct = "UA-77224-4";
   urchinTracker();
   </script>  
-<?php } ?>
+<?php endif ?>
