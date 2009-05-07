699e37902f4a065bd63266c857c4ad12b4b8e9d9
Ryan Carver <rcarver@eames.local>
2009-05-06 22:02:01 -0700

    add rio nido in Towns, add Greg, remove Roof

--- shared/nav.php
+++ shared/nav.php
@@ -2,6 +2,7 @@
   <ol>
     <li class="<?php if ($current == "home") { echo "current"; } ?>"><a href="index.php">Home</a></li>
     <li class="<?php if ($current == "city") { echo "current"; } ?>"><a href="city.php">City</a></li>
+    <li class="<?php if ($current == "town") { echo "current"; } ?>"><a href="town.php">Town</a></li>
     <li class="<?php if ($current == "coast") { echo "current"; } ?>"><a href="coast.php">Coast</a></li>
     <li class="<?php if ($current == "lake") { echo "current"; } ?>"><a href="lake.php">Lake</a></li>
     <li class="<?php if ($current == "friends") { echo "current"; } ?>"><a href="friends.php">Friends</a></li>
---
+++ town.php
@@ -0,0 +1,22 @@
+<?php
+  require 'shared/gallery.class.php';
+  $gallery = new Gallery("images/photos/town", $_GET["i"]);
+  $current = "town";
+?>
+<!html>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <title>Ryan Carver Photography</title>
+  <link type="text/css" rel="stylesheet" href="stylesheets/main.css">
+</head>
+<body id="index">
+  <div id="wrapper">
+    <div class="grid"></div>
+    <?php include 'shared/header.php' ?>
+    <?php include 'shared/nav.php' ?>
+    <?php include 'shared/gallery.php' ?>
+    <?php include 'shared/footer.php' ?>
+  </div>
+  <?php include 'shared/analytics.php' ?>
+</body>
+</html>
